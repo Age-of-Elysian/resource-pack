1dd0e78951665f620f9c8770f1d4a104a19c58b8
Vrganj <43708436+Vrganj@users.noreply.github.com>
2025-11-15 15:27:13 +0100

    add script to sync resource packs to the webserver

---
+++ sync.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# --- Generate resource packs ---
+jar_name="slimepack-standalone-2.12-SNAPSHOT-all.jar"
+versions=("1.21.10" "1.21.9" "1.21.8" "1.21.7" "1.21.6" "1.21.5" "1.21.4")
+
+for version in "${versions[@]}"; do
+    java -jar $jar_name $version
+done
+
+# --- Upload files ---
+sftp "website@ageofelysian.com" <<EOF
+cd website/static/resource-pack
+$(for version in "${versions[@]}"; do
+    echo "put generated/resource-pack-$version.zip"
+done)
+ls
+bye
+EOF
+
+echo "Upload finished."
