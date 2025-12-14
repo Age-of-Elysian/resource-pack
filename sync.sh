3432d77fb06ff2782b8e4123e77016d3ba03a84f
Vrganj <43708436+Vrganj@users.noreply.github.com>
2025-12-14 15:56:46 +0100

    update to 1.21.11

--- sync.sh
+++ sync.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # --- Generate resource packs ---
-jar_name="slimepack-standalone-2.22-SNAPSHOT-all.jar"
-versions=("1.21.10" "1.21.9" "1.21.8" "1.21.7" "1.21.6" "1.21.5" "1.21.4")
+jar_name="slimepack-standalone-2.23-SNAPSHOT-all.jar"
+versions=("1.21.11" "1.21.10" "1.21.9" "1.21.8" "1.21.7" "1.21.6" "1.21.5" "1.21.4")
 
 for version in "${versions[@]}"; do
     java -jar $jar_name $version
